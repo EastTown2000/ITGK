b6dd5dcc1dedf5fc3948bb97379054e758494dab
EastTown2000 <sindreostby@gmail.com>
2020-11-12 17:44:55 +0100

    Chess new update

--- Øvinger/Øving 10/chess.py
+++ Øvinger/Øving 10/chess.py
@@ -1,8 +1,7 @@
 class Piece:
-    def __init__(self, side, type, position_x, position_y):
+    def __init__(self, side, position_x, position_y):
         self.is_white = side.lower() == 'white'
         self.is_alive = True
-        self.type = type
         self.y = position_y
         self.x = position_x
     
@@ -12,6 +11,9 @@ class Piece:
             return piece.is_white != self.is_white
         else:
             return False
+    
+    def __str__(self):
+        return str(type(self))[17: -2]
 
 class Pawn(Piece):
     def posible_moves(self):
@@ -109,7 +111,7 @@ class Bishop(Piece):
             elif dir_x == '-':
                 new_x = x - i
             
-            if dir_y == 'y':
+            if dir_y == '+':
                 new_y = y + i
             elif dir_y == '-':
                 new_y = x - i
@@ -162,7 +164,7 @@ class Queen(Piece):
             elif dir_x == '-':
                 new_x = x - i
             
-            if dir_y == 'y':
+            if dir_y == '+':
                 new_y = y + i
             elif dir_y == '-':
                 new_y = x - i
@@ -220,6 +222,22 @@ class Knight(Piece):
         possible_moves = []
         x = self.x
         y = self.y
+        
+        def knight_search(start_x, start_y):
+            move_types = [(-2, -1), (-2, +1), (+2, -1), (+2, +1), (-1, -2), (-1, +2), (+1, -2), (+1, +2)]
+            possible_positions = []
+            for (x, y) in move_types:
+                x_possible = start_x + x
+                y_possible = start_y + y
+                if 0 < x_possible <= 8 and 0 < y_possible <= 8:
+                    possible_positions.append([x_possible, y_possible])
+            return possible_positions
+        searchable_spot = knight_search(x, y)
+        
+        for search in searchable_spot:
+            search_piece = board_instance.get_piece_at(*search)
+            if search_piece == None or self.is_opposite_piece_at(*search):
+                possible_moves.append(search)
         return possible_moves
 
 class King(Piece):
@@ -227,24 +245,66 @@ class King(Piece):
         possible_moves = []
         x = self.x
         y = self.y
+        
+        def king_search(start_x, start_y):
+            move_types = [(-1, -1), (-1, 0), (-1, +1), (0, +1), (0, -1), (+1, +1), (+1, 0), (+1, -1)]
+            possible_positions = []
+            for (x, y) in move_types:
+                x_possible = start_x + x
+                y_possible = start_y + y
+                if 0 < x_possible <= 8 and 0 < y_possible <= 8:
+                    possible_positions.append([x_possible, y_possible])
+            return possible_positions
+        searchable_spot = king_search(x, y)
+        
+        for search in searchable_spot:
+            search_piece = board_instance.get_piece_at(*search)
+            if search_piece == None or self.is_opposite_piece_at(*search):
+                possible_moves.append(search)
+        
         return possible_moves
 
 class Board:
     def __init__(self):
-        self.board=[[Rook('white', Rook, 1, 1), Knight('white', Knight, 2, 1), Bishop('white', Bishop, 3, 1), Queen('white', Queen, 4, 1), King('white', King, 5, 1), Bishop('white', Bishop, 6, 1), Knight('white', Knight, 7, 1), Rook('white', Rook, 8, 1)],
-                    [Pawn('white', Pawn, 1, 2), Pawn('white', Pawn, 2, 2), Pawn('white', Pawn, 3, 2), Pawn('white', Pawn, 4, 2), Pawn('white', Pawn, 5, 2), Pawn('white', Pawn, 6, 2), Pawn('white', Pawn, 7, 2), Pawn('white', Pawn, 8, 2)], 
+        self.board=[[Rook('white', 1, 1), Knight('white', 2, 1), Bishop('white', 3, 1), Queen('white', 4, 1), King('white', 5, 1), Bishop('white', 6, 1), Knight('white', 7, 1), Rook('white', 8, 1)],
+                    [Pawn('white', 1, 2), Pawn('white', 2, 2), Pawn('white', 3, 2), Pawn('white', 4, 2), Pawn('white', 5, 2), Pawn('white', 6, 2), Pawn('white', 7, 2), Pawn('white', 8, 2)], 
                     [None, None, None, None, None, None, None, None], 
                     [None, None, None, None, None, None, None, None], 
                     [None, None, None, None, None, None, None, None], 
                     [None, None, None, None, None, None, None, None], 
-                    [Pawn('black', Pawn, 1, 7), Pawn('black', Pawn, 2, 7), Pawn('black', Pawn, 3, 7), Pawn('black', Pawn, 4, 7), Pawn('black', Pawn, 5, 7), Pawn('black', Pawn, 6, 7), Pawn('black', Pawn, 7, 7), Pawn('black', Pawn, 8, 7)], 
-                    [Rook('black', Rook, 1, 8), Knight('black', Knight, 2, 8), Bishop('black', Bishop, 3, 8), Queen('black', Queen, 4, 8), King('black', King, 5, 8), Bishop('black', Bishop, 6, 8), Knight('black', Knight, 7, 8), Rook('black', Rook, 8, 8)]]    
-    
-    def draw(self):
-        pass
+                    [Pawn('black', 1, 7), Pawn('black', 2, 7), Pawn('black', 3, 7), Pawn('black', 4, 7), Pawn('black', 5, 7), Pawn('black', 6, 7), Pawn('black', 7, 7), Pawn('black', 8, 7)], 
+                    [Rook('black', 1, 8), Knight('black', 2, 8), Bishop('black', 3, 8), Queen('black', 4, 8), King('black', 5, 8), Bishop('black', 6, 8), Knight('black', 7, 8), Rook('black', 8, 8)]]    
     
     def get_piece_at(self, x, y):
         return self.board[y-1][x-1]
+    
+    def draw(self):
+        piece_symbols = {'Pawn': ['♙', '♟'], 'Rook': ['♖', '♜'], 'Bishop': ['♗', '♝'], 'Knight': ['♘', '♞'], 'Queen': ['♕', '♛'], 'King': ['♔', '♚']}
+        def symbol_at(x, y):
+            piece = self.get_piece_at(x, y)
+            if piece == None:
+                return ' '
+            else:
+                return piece_symbols[str(piece)][piece.is_white]
+
+        print(' ┌───┬───┬───┬───┬───┬───┬───┬───┐\n'
+              f'8│ {symbol_at(1,8)} │ {symbol_at(2,8)} │ {symbol_at(3,8)} │ {symbol_at(4,8)} │ {symbol_at(5,8)} │ {symbol_at(6,8)} │ {symbol_at(7,8)} │ {symbol_at(8,8)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'7│ {symbol_at(1,7)} │ {symbol_at(2,7)} │ {symbol_at(3,7)} │ {symbol_at(4,7)} │ {symbol_at(5,7)} │ {symbol_at(6,7)} │ {symbol_at(7,7)} │ {symbol_at(8,7)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'6│ {symbol_at(1,6)} │ {symbol_at(2,6)} │ {symbol_at(3,6)} │ {symbol_at(4,6)} │ {symbol_at(5,6)} │ {symbol_at(6,6)} │ {symbol_at(7,6)} │ {symbol_at(8,6)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'5│ {symbol_at(1,5)} │ {symbol_at(2,5)} │ {symbol_at(3,5)} │ {symbol_at(4,5)} │ {symbol_at(5,5)} │ {symbol_at(6,5)} │ {symbol_at(7,5)} │ {symbol_at(8,5)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'4│ {symbol_at(1,4)} │ {symbol_at(2,4)} │ {symbol_at(3,4)} │ {symbol_at(4,4)} │ {symbol_at(5,4)} │ {symbol_at(6,4)} │ {symbol_at(7,4)} │ {symbol_at(8,4)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'3│ {symbol_at(1,3)} │ {symbol_at(2,3)} │ {symbol_at(3,3)} │ {symbol_at(4,3)} │ {symbol_at(5,3)} │ {symbol_at(6,3)} │ {symbol_at(7,3)} │ {symbol_at(8,3)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'2│ {symbol_at(1,2)} │ {symbol_at(2,2)} │ {symbol_at(3,2)} │ {symbol_at(4,2)} │ {symbol_at(5,2)} │ {symbol_at(6,2)} │ {symbol_at(7,2)} │ {symbol_at(8,2)} │\n'
+              ' ├───┼───┼───┼───┼───┼───┼───┼───┤\n'
+              f'1│ {symbol_at(1,1)} │ {symbol_at(2,1)} │ {symbol_at(3,1)} │ {symbol_at(4,1)} │ {symbol_at(5,1)} │ {symbol_at(6,1)} │ {symbol_at(7,1)} │ {symbol_at(8,1)} │\n'
+              ' └───┴───┴───┴───┴───┴───┴───┴───┘\n'
+              '   1   2   3   4   5   6   7   8\n')
     
     def move(self, start, end):
         piece = self.get_piece_at(*start)
@@ -267,22 +327,21 @@ class Game:
     def __init__(self):
         self.is_done = False
         self.winner = None    
-    def player_tunrn(self, player_to_move):
+    def player_move(self, player_to_move):
         pass
     def is_in_check(self, player_to_move):
         pass
     def over(self, player_to_move, score):
         self.is_done = True
 
-#for å være i sjakk, sjekker om spilleren er i sjakk, hvis den ar det printes 'You are in check form (piece symbol)
-#at (x, y)'
+# for å være i sjakk, sjekker om spilleren er i sjakk, hvis den ar det printes 'You are in check form (piece symbol)
+# at (x, y)'
 if __name__ == "__main__":
     board_instance = Board()
     game_inatance = Game()
     players_turn = True
     #True is white False is black for players_turn
     while game_inatance.is_done == False:
-        game_inatance.player_turn(players_turn)
+        game_inatance.player_move(players_turn)
         players_turn = not players_turn
     print(f'{game_inatance.winner} is the Winner')
-    
